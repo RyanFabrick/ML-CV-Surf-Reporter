c16a91be8bb6b5117a6e3ddccb0604211d820696
Ryan Fabrick <ryanfabrick@gmail.com>
2025-06-19 17:30:39 -0700

    API data feature expansion and frontend minor metrics display design - PoC continued

--- api_maybe.py
+++ api_maybe.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import send_file
+from flask import render_template
 from datetime import datetime, timezone
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def serve_frontend():
 
-    return send_file('frontend.html')
+    return render_template('frontend.html')
 
 #main shit
 @app.route('/api/surfdata')
@@ -29,9 +30,20 @@ def get_surf_data():
         good = ds['waveFlagPrimary'] == 1
 
         # Step 3: Extract relevant data
-        # Relevant data: time, wave height significant (waveHs)
+        # Relevant data: time, 
+        # wave height significant (waveHs),
+        # peak wave period (waveTp),
+        # peak wave direction (waveDp),
+        # avg wave period (waveTa),
+        # mean zero-upcrossing period (waveTz),
+        # peak wave power spectral density (wavePeakPSD)
         times = ds['waveTime'].values[good]
         wave_height_signficant = ds['waveHs'].values[good]
+        wave_tp = ds['waveTp'].values[good]
+        wave_dp = ds['waveDp'].values[good]
+        wave_ta = ds['waveTa'].values[good]
+        wave_tz = ds['waveTz'].values[good]
+        wave_psd = ds['wavePeakPSD'].values[good]
 
         # Step 4: convert UNIX to readable format
 
@@ -45,7 +57,12 @@ def get_surf_data():
         return jsonify({
 
             'time': readable_time,
-            "waveHs": wave_height_signficant[:10].tolist()
+            "waveHs": wave_height_signficant[:10].tolist(),
+            "waveTp": wave_tp[:10].tolist(),
+            "waveDp": wave_dp[:10].tolist(),
+            "waveTa": wave_ta[:10].tolist(),
+            "waveTz": wave_tz[:10].tolist(),
+            "wavePeakPSD": wave_psd[:10].tolist()
 
         })
         
