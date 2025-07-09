0e6199ecdfde1b1d2be75142aa54dfa51e955eab
Ryan Fabrick <ryanfabrick@gmail.com>
2025-07-08 17:53:25 -0700

    WIP: About page with informative info on application

--- frontend/src/components/About_Page.js
+++ frontend/src/components/About_Page.js
@@ -10,9 +10,126 @@ const AboutPage = ({ onNavigate }) => (
     </div>
     <div className="page-content">
       <h2>About This Website</h2>
-      <p>about stuff here</p>
+      <p>
+        Write stuff here
+      </p>
+      
+      <div className="settings-section">
+        <h3>Computer Vision Works</h3>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Real-time Surfer Detection</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Computer Vision & Machine Learning Elements</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Stream Processing</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="settings-section">
+        <h3>Data Metric Meanings</h3>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Wave Height (Hs)</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Peak Period (Tp)</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Wave Direction (Dp)</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Average Period (Ta)</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Zero Crossing Period (Tz)</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">Peak PSD</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="settings-section">
+        <h3>Data Sources</h3>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">write stuff here ocean data</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">write stuff here surfcam data</div>
+            <p className="setting-description">
+              write stuff here
+            </p>
+          </div>
+        </div>
+      </div>
+
+      
+      <div className="settings-section">
+        <h3>Public GitHub Repository</h3>
+        <div className="setting-item">
+          <div>
+            <div className="setting-label">GitHub Repository Link</div>
+            <p className="setting-description">
+              repo link here
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+
+
 );
 
 export default AboutPage;
